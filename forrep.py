265665749caf20ec4048e6d8b5adb478f6f67254
IrinaPavlova <ispavlovais@gmail.com>
2016-02-19 00:03:13 +0300

    parsing started

--- forrep.py
+++ forrep.py
@@ -9,7 +9,7 @@ def url_open(url):
     urlAdress = url
     html = urlr.urlopen(urlAdress)
     html = html.read().decode('utf-8')
-    return(html)
+    return html
 
 
 def articles_links(author):
@@ -19,11 +19,30 @@ def articles_links(author):
         if 'articles/music/review' in str(link):
             yield(link.get('href'))
 
+
+def count_pages(author):
+    page = url_open('http://www.rollingstone.ru/authors/' + author)
+    c = re.findall('<li><a href="/authors/6/page/.*?/">(.*?)</a>', page)
+    return c
+
+
+def articles_links_more(author):
+    for n in count_pages(author):
+        page = url_open('http://www.rollingstone.ru/authors/' + author + '/page/' + n)
+        soup = BeautifulSoup(page, 'html.parser')
+        for link in soup.find_all('a'):
+            if 'articles/music/review' in str(link):
+                yield(link.get('href'))
+
+
 links_6 = []
 
 for i in articles_links('6'):
     if i not in links_6:
         links_6.append(i)
+for i in articles_links_more('6'):
+    if i not in links_6:
+        links_6.append(i)
 
-print(links_6)
+print(len(links_6), links_6)
 
