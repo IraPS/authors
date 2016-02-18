23134cf62d770211a2bafe1ad8c35ed1c15c22d3
IrinaPavlova <ispavlovais@gmail.com>
2016-02-18 23:26:22 +0300

    parsing started

--- forrep.py
+++ forrep.py
@@ -2,8 +2,25 @@
 
 import urllib.request as urlr
 import re
+from bs4 import BeautifulSoup
 
-urlAdress = 'http://www.rollingstone.ru/authors/2523/'
-html = urlr.urlopen(urlAdress)
-html = html.read().decode('utf-8')
-print(type(html))
+
+
+def do(url):
+    urlAdress = url
+    html = urlr.urlopen(urlAdress)
+    html = html.read().decode('utf-8')
+    return(html)
+
+
+author = '2523/'
+authorurl = 'http://www.rollingstone.ru/authors/' + author
+res = do(authorurl)
+links = re.findall('<span class="href-more">\|<a href="/articles(.*?)">', res)
+#for i in links:
+    #print('YES')
+    #print(do('http://www.rollingstone.ru'+i))
+
+soup = BeautifulSoup(res, 'html.parser')
+print(soup.prettify())
+
