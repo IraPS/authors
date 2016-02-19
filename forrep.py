9723d978703c272db6cffdb0f5d65bcf941396ce
IrinaPavlova <ispavlovais@gmail.com>
2016-02-19 13:52:32 +0300

    Готово (2)

--- forrep.py
+++ forrep.py
@@ -5,6 +5,7 @@
 import urllib.request as urlr
 import re
 from bs4 import BeautifulSoup
+import os
 
 
 def url_open(url):
@@ -15,26 +16,17 @@ def url_open(url):
     return html
 
 
-def articles_links(author):
-    '''Функция достает ссылки на рецензии автора с его первой страницы'''
-    page = url_open('http://www.rollingstone.ru/authors/' + author)
-    soup = BeautifulSoup(page, 'html.parser')
-    for link in soup.find_all('a'):
-        if 'articles/music/review' in str(link):
-            yield(link.get('href'))
-
-
 def count_pages(author):
     '''Функция считает количество страниц для перелистывания списка рецензий автора'''
     page = url_open('http://www.rollingstone.ru/authors/' + author)
     c = re.findall('<li><a href="/authors/' + author + '/page/.*?/">(.*?)</a>', page)
-    return c
+    return int(c[-1])
 
 
 def articles_links_more(author):
-    '''Функция достает ссылки на рецензии автора с последующих страниц'''
-    for n in count_pages(author):
-        page = url_open('http://www.rollingstone.ru/authors/' + author + '/page/' + n)
+    '''Функция достает ссылки на рецензии автора со всех страниц'''
+    for n in range(1, count_pages(author)+1):
+        page = url_open('http://www.rollingstone.ru/authors/' + author + '/page/' + str(n))
         soup = BeautifulSoup(page, 'html.parser')
         for link in soup.find_all('a'):
             if 'articles/music/review' in str(link):
@@ -44,31 +36,62 @@ def articles_links_more(author):
 def articles_of_author(author):
     '''Функция возвращает массив со список ссылок на рецензии автора'''
     links = []
-    for i in articles_links(author):
-        if i not in links:
-            links.append(i)
     for k in articles_links_more(author):
         if k not in links:
             links.append(k)
-    return links
+    return len(links), links
 
 
 def articles_texts(author):
-    '''Функция возвращает тексты всех рецензий автора'''
-    for articleLink in articles_of_author(author):
-        page = url_open('http://www.rollingstone.ru'+articleLink)
-        soup = BeautifulSoup(page, 'html.parser')
-        yield(soup.find('div', {'class': 'block-content'}).getText())
+    '''Функция возвращает тексты всех рецензий автора , если их меньше 100 (?)'''
+    if articles_of_author(author)[0] <= 100 and articles_of_author(author)[0] > 50:
+        new_dir = './articles/' + author
+        os.makedirs(os.path.join(new_dir))
+        for articleLink in articles_of_author(author)[1]:
+            page = url_open('http://www.rollingstone.ru' + articleLink)
+            soup = BeautifulSoup(page, 'html.parser')
+            yield(soup.find('div', {'class': 'block-content'}).getText())
 
 
 def save_texts(author):
     '''Функция сохраняет в файлы тексты авторов'''
     number = 1
     for i in articles_texts(author):
-        file = open('./articles/' + author + '_' + str(number) + '.txt', 'w', encoding='utf-8')
+        file = open('./articles/' + author + '/' + str(number) + '.txt', 'w', encoding='utf-8')
         file.write(i)
         file.close()
         number += 1
+
+
+def authors():
+    '''Функция ищет id авторов рецензий'''
+    authors = []
+    articles_to_get_authors = []
+    for n in range(1, 3):
+        magazine = 'http://www.rollingstone.ru/music/review/page/' + str(n)
+        magazine = url_open(magazine)
+        soup = BeautifulSoup(magazine, 'html.parser')
+        for link in soup.find_all('a'):
+            if 'articles/music/review' in str(link):
+                if link.get('href') not in articles_to_get_authors:
+                    articles_to_get_authors.append(link.get('href'))
+    for articleLink in articles_to_get_authors:
+        article = url_open('http://www.rollingstone.ru' + articleLink)
+        soup = BeautifulSoup(article, 'html.parser')
+        span = soup.find('span', {'class': 'black-bold'})
+        author = re.search('<a href="/authors/(.*?)/">', str(span)).group(1)
+        if author not in authors:
+            authors.append(author)
+    return authors
+
+
+for id in authors():
+    save_texts(id)
+
+
+
+
+
 
 
 # Пример работы: просто пишем save_texts и id автора
