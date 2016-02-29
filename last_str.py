409392e10ce6f2a4c4eb064c2c4cabdc898eadad
IrinaPavlova <ispavlovais@gmail.com>
2016-02-29 12:50:12 +0300

    удаление последней строки

--- last_str.py
+++ last_str.py
@@ -1,6 +1,15 @@
-__author__ = 'IrinaPavlova'
-
 import os
 
-for root, dirs, files in os.walk('./articles'):
-
+for root, dirs, files in os.walk('./articles1'):
+    for file in files:
+        if file.endswith('.txt'):
+            f = open('./articles1/' + file, 'r', encoding='utf-8')
+            f0 = f.readlines()
+            f1 = list()
+            f1.append(f0[:-1])
+            f.close()
+            f = open('./articles1/' + file + '_new.txt', 'w', encoding='utf-8')
+            for i in f1:
+                for u in i:
+                    f.write(u)
+            f.close()
