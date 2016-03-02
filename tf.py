6c94467d2c608a526e839e9bbd912feb55f1e84a
Natasha Akutina <akutinabr12@gmail.com>
2016-03-02 03:18:20 +0300

    итоговая программа (в нее можно добавить еще частей речи)
    и сократить признаки (сейчас их более 13000)

--- tf.py
+++ tf.py
@@ -7,11 +7,12 @@ tf_idf1 = {}
 corpus1 = []
 mass = {} #делить на кол-во слов в документе
 
+
 """
 def lemmas(file):
-    os.system('/Users/akutinanatasha/Desktop/Python/module4/artic/' + 'mystem -d -n -i -s --format=json --eng-gr ' + '/Users/akutinanatasha/Desktop/Python/module4/artic/784/' + file + ' ' + '/Users/akutinanatasha/Desktop/Python/module4/artic/784/' + 'lemma' + file)
+    os.system('/Users/akutinanatasha/Desktop/Python/module4/artic/' + 'mystem -d -n -i -s --format=json --eng-gr ' + '/Users/akutinanatasha/Desktop/Python/module4/artic/763_54/' + file + ' ' + '/Users/akutinanatasha/Desktop/Python/module4/artic/763_54/' + 'lemma' + file)
 
-for root, dirs, files in os.walk('./784'):
+for root, dirs, files in os.walk('./763_54'):
     for file in files:
         if file.endswith('.txt'):
             lemmas(file)
@@ -49,33 +50,50 @@ def tfidf(i):
     return(round((mass[i]*idf), 2)) #формула?
 
 
-for root, dirs, files in os.walk('./1830'):
-    for file in files:
-        if file.startswith('lemma'):
-            with open('./1830/' + file, 'r', encoding='utf-8') as f:
-                author1 = f.read()
-                a_texts = re.split(r'text.+\\s"}', author1)
-                k = re.findall('(?<=analysis"\:\[\{"lex"\:")[а-я]+', author1) #повторы?
-                for i in k:
-                    if i not in mass:
-                        mass[i] = 1
-                    else:
-                        s = mass[i] + 1
-                        mass[i] = s
-                for p in a_texts:
-                    print(p)
-                    n = re.findall('(?<=analysis"\:\[\{"lex"\:")[а-я]+', p)
-                    print(n)
-                    j = [tfidf(w) for w in n]
-                    print(j)
-                    print(pril(p), sush(p), glag(p), nar(p), mest(p))
-                    corpus1.append([pril(p), sush(p), glag(p), nar(p), mest(p), j])
-        mass = {}
+ids = ['103_95', '141_70', '1830_36', '2510_49', '2523_63', '53_50', '557_71', '6_100', '763_54']
+for x in ids:
+    outfile = open('./' + x + '.csv', 'w')
+    for root, dirs, files in os.walk('./' + x):
+        for file in files:
+            if file.startswith('lemma'):
+                with open('./' + x + '/' + file, 'r', encoding='utf-8') as f:
+                    author1 = f.read()
+                    a_texts = re.split(r'text.+\\s"}', author1)
+                    k = re.findall('(?<=analysis"\:\[\{"lex"\:")[а-я]+', author1) #повторы?
+                    for i in k:
+                        if i not in mass:
+                            mass[i] = 1
+                        else:
+                            s = mass[i] + 1
+                            mass[i] = s
+                    for p in a_texts:
+                        cor = []
+                        n = re.findall('(?<=analysis"\:\[\{"lex"\:")[а-я]+', p)
+                        infile = open('./1.csv', 'r')
+                        for j in infile:
+                            j = j.strip()
+                            j = j.split(';')
+                            for k in n:
+                                if k == j[0]:
+                                    cor.append(tfidf(k))
+                                else:
+                                    cor.append(0)
+                        cor.append(pril(p))
+                        cor.append(sush(p))
+                        cor.append(glag(p))
+                        cor.append(nar(p))
+                        cor.append(mest(p))
+                        corpus1.append(cor)
+                        for i in cor:
+                            i = str(i)
+                            outfile.write(i + ', ')
+                        outfile.write('\n')
+            mass = {}
 
 #print(k)
-print(corpus1)
+#print(corpus1)
 
-
+outfile.close()
 
 
 
