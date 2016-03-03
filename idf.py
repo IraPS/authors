a0d83f4686761ca4cba828b6c201b4bd1901c40e
Natasha Akutina <akutinabr12@gmail.com>
2016-03-03 16:43:02 +0300

    все последние файлы

--- idf.py
+++ idf.py
@@ -1,8 +1,5 @@
 __author__ = 'akutinanatasha'
 
-# пробуем гит у Наташ в пайчарм
-
-
 import os,re,math
 lemmas1 = set()
 idf1 = {}
@@ -18,15 +15,18 @@ for root, dirs, files in os.walk('./all'):
             with open('./all/' + file, 'r', encoding='utf-8') as f:
                 author1 = f.read()
                 k = re.findall('(?<=analysis"\:\[\{"lex"\:")[а-я]+', author1)
+                k = set(k)
                 for i in k:
                     if i not in slovar:
-                        slovar[i] = 1
+                        t = 1
                     else:
                         t = slovar[i] + 1
-                        slovar[i] = t
+                    slovar[i] = t
+
+print(slovar)
 
 for i in slovar:
-    slovar_itog[i] = round(math.log10(586/slovar[i]),2)
+    slovar_itog[i] = round(math.log10(529/slovar[i]), 3) #idf
 
 for i in slovar_itog:
     outfile.write(i + ';' + str(slovar_itog[i]))
--- tf.py
+++ tf.py
@@ -1,9 +1,6 @@
 __author__ = 'akutinanatasha'
 
 import os,re,math
-lemmas1 = set()
-idf1 = {}
-tf_idf1 = {}
 corpus1 = []
 mass = {} #делить на кол-во слов в документе
 
@@ -20,37 +17,69 @@ for root, dirs, files in os.walk('./763_54'):
 
 #предлоги можно еще считать?
 
-def pril(sent):
-    k = re.findall('analysis.*?gr.*?(A|ANUM)[^P]', sent)
+def pril(sent):  #12
+    k = re.findall('analysis.*?gr.*?(A)[^P|^D|^N]', sent)
     return(len(k))
 
-def sush(sent):
+def nar(sent): #1
+    k = re.findall('analysis.*?gr.*?(ADV|ADVPRO)', sent)
+    return(len(k))
+
+def chispril(sent): #0
+    k = re.findall('analysis.*?gr.*?(ANUM)', sent)
+    return(len(k))
+
+def mestpril(sent): #1
+    k = re.findall('analysis.*?gr.*?(APRO)', sent)
+    return(len(k))
+
+def chkomp(sent): #0
+    k = re.findall('analysis.*?gr.*?(COM)', sent)
+    return(len(k))
+
+def sojuz(sent): #2
+    k = re.findall('analysis.*?gr.*?(CONJ)', sent)
+    return(len(k))
+
+def intj(sent): #0
+    k = re.findall('analysis.*?gr.*?(INTJ)', sent)
+    return(len(k))
+
+def num(sent): #0
+    k = re.findall('analysis.*?gr.*?[^A](NUM)', sent)
+    return(len(k))
+
+def part(sent): #2
+    k = re.findall('analysis.*?gr.*?(PART)', sent)
+    return(len(k))
+
+def predl(sent): #5
+    k = re.findall('analysis.*?gr.*?[^ADV|^A](PR)[^O]', sent)
+    return(len(k))
+
+def sush(sent): #17
     k = re.findall('analysis.*?gr.*?(S)[^P]', sent)
     return(len(k))
 
-def glag(sent):
-    k = re.findall('analysis.*?gr.*?(V)[^P]', sent)
+def glag(sent): #4
+    k = re.findall('analysis.*?gr.*?[^D](V)[^P]', sent)
     return(len(k))
 
-def nar(sent):
-    k = re.findall('analysis.*?gr.*?(ADV|ADVPRO)[^P]}', sent)
+def mestsush(sent): #0
+    k = re.findall('analysis.*?gr.*?(SPRO)', sent)
     return(len(k))
 
-def mest(sent):
-    k = re.findall('analysis.*?gr.*?(SPRO)[^P]', sent)
-    return(len(k))
-
-def tfidf(i):
+def tfidf(i, col):
     infile = open('./1.csv', 'r')
     for j in infile:
         j = j.split(';')
         if i == j[0]:
             idf = j[1].strip()
             idf = float(idf)
-    return(round((mass[i]*idf), 2)) #формула?
+    return(round(((mass[i]/col)*idf), 3)) #col - кол-во слов в документе
 
 
-ids = ['103_95', '141_70', '1830_36', '2510_49', '2523_63', '53_50', '557_71', '6_100', '763_54']
+ids = ['1830_36', '141_70', '103_95', '2510_49', '2523_63', '53_50', '557_71', '6_100', '763_54']
 for x in ids:
     outfile = open('./' + x + '.csv', 'w')
     for root, dirs, files in os.walk('./' + x):
@@ -58,15 +87,15 @@ for x in ids:
             if file.startswith('lemma'):
                 with open('./' + x + '/' + file, 'r', encoding='utf-8') as f:
                     author1 = f.read()
+                    k2 = re.findall('(?<=analysis"\:\[\{"lex"\:")[а-я]+', author1) #кол-во слов в документе
                     a_texts = re.split(r'text.+\\s"}', author1)
-                    k = re.findall('(?<=analysis"\:\[\{"lex"\:")[а-я]+', author1) #повторы?
-                    for i in k:
+                    for i in k2:
                         if i not in mass:
                             mass[i] = 1
                         else:
                             s = mass[i] + 1
                             mass[i] = s
-                    for p in a_texts:
+                    for p in a_texts: ###
                         cor = []
                         n = re.findall('(?<=analysis"\:\[\{"lex"\:")[а-я]+', p)
                         infile = open('./1.csv', 'r')
@@ -75,19 +104,20 @@ for x in ids:
                             j = j.split(';')
                             for k in n:
                                 if k == j[0]:
-                                    cor.append(tfidf(k))
+                                    cor.append(tfidf(k, len(k2)))
                                 else:
                                     cor.append(0)
-                        cor.append(pril(p))
-                        cor.append(sush(p))
-                        cor.append(glag(p))
-                        cor.append(nar(p))
-                        cor.append(mest(p))
+                        b = [pril(p), nar(p), chispril(p), mestpril(p), chkomp(p), sojuz(p), intj(p), num(p),
+                             part(p), predl(p), sush(p), glag(p), mestsush(p)]
+                        for i in b:
+                            cor.append(i)
                         corpus1.append(cor)
                         for i in cor:
                             i = str(i)
                             outfile.write(i + ', ')
                         outfile.write('\n')
+                #print(p)
+            #print(mass)
             mass = {}
 
 #print(k)
